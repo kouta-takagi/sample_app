db4895cac73c1df6680cf1657f22d2cd92dbcc9c
39takagikouta <kouta.takagi0728@gmail.com>
2024-08-29 22:53:40 +0900

    テスト作成

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,9 @@
+# require "test_helper"
+
+# class ApplicationHelperTest < ActionDispatch::IntegrationTest
+#   test "full title helper" do
+#     get help_url
+#     # assert_equal :title, full_title
+#     assert_equal :title, full_title("Help")
+#   end
+# end
