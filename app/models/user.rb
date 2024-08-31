640d221ecd922bb09021fa3e0d243aede91f3384
39takagikouta <kouta.takagi0728@gmail.com>
2024-09-01 03:01:02 +0900

    make a basic user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  before_save {email.downcase!}
   validates :name, presence: true, length: {maximum: 50}
-  validates :email, presence: true, length: {maximum: 255}
-  validates :email, format: { with: /<regular expression>/ }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255}, uniqueness: true
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: {minimum: 6}, presence: true
+
+  has_secure_password
 end
