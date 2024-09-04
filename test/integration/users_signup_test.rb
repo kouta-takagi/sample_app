99f152f5a8a95fa5c257fefb0a01dd325800f12c
kouta-takagi <k-takagi@ruby-dev.jp>
2024-09-04 09:54:54 +0900

    7賞終了

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
   end
+  test 'valid signup infomation' do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'kouta',
+                                         email: 'user@example.com', password: 'foobar',
+                                         password_confirmation: 'foobar' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash[:notice] == 'Welcome to the Sample App!'
+  end
 end
